eb8e5a955d79351c3bd8444f17436f48d6ca14e7
Unknown <joaoxp@gmail.com>
2019-03-22 15:11:16 +0100

    Adds analysis placeholders

---
+++ analysis/avalanche.py
@@ -0,0 +1,9 @@
+"""
+Module for the avalanche analysis of MEA datasets.
+
+"""
+# -*- coding: utf-8 -*-
+# @Author: joaopn
+# @Date:   2019-03-22 12:54:07
+# @Last Modified by:   joaopn
+# @Last Modified time: 2019-03-22 13:14:47
---
+++ analysis/utils.py
@@ -0,0 +1,10 @@
+"""
+Module for utilities.
+
+"""
+# -*- coding: utf-8 -*-
+# @Author: joaopn
+# @Date:   2019-03-22 12:58:11
+# @Last Modified by:   joaopn
+# @Last Modified time: 2019-03-22 13:16:54
+
