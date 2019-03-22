46ecea1a6357040693614dea641b2c2e825a5bbe
Unknown <joaoxp@gmail.com>
2019-03-22 15:11:16 +0100

    Adds analysis placeholders

---
+++ analysis/filtering.py
@@ -0,0 +1,9 @@
+"""
+Module for filtering timeseries functions.
+
+"""
+# -*- coding: utf-8 -*-
+# @Author: joaopn
+# @Date:   2019-03-22 12:58:33
+# @Last Modified by:   joaopn
+# @Last Modified time: 2019-03-22 14:44:56
---
+++ analysis/fitting.py
@@ -0,0 +1,9 @@
+"""
+Module for fitting functions.
+
+"""
+# -*- coding: utf-8 -*-
+# @Author: joaopn
+# @Date:   2019-03-22 12:58:22
+# @Last Modified by:   joaopn
+# @Last Modified time: 2019-03-22 14:44:19
